b3ab81d8e4c5a8763f5e66e9a0c11e2ff7bbec95
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-10-25 07:55:02 -0300

    chore: setup project

---
+++ src/main/java/br/com/fag/seektweets/SeekTweetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fag.seektweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekTweetsApplication.class, args);
+	}
+
+}
