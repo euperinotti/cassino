495c2325106f84c184a741b07651e3248b8dd164
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-10-31 20:52:53 -0300

    feat: add dto folder

---
+++ backend/src/main/java/br/com/fag/cassino/domain/dto/ContaDTO.java
@@ -0,0 +1,42 @@
+package br.com.fag.cassino.domain.dto;
+
+public class ContaDTO {
+  private Float valorGasto;
+  private Float valorAdquirido;
+  private Float retornoFinal;
+
+  public ContaDTO() {
+  }
+
+  public ContaDTO(Float valorGasto, Float valorAdquirido, Float retornoFinal) {
+    this.valorGasto = valorGasto;
+    this.valorAdquirido = valorAdquirido;
+    this.retornoFinal = retornoFinal;
+  }
+
+  public Float getValorGasto() {
+    return this.valorGasto;
+  }
+
+  public void setValorGasto(Float valorGasto) {
+    this.valorGasto = valorGasto;
+  }
+
+  public Float getValorAdquirido() {
+    return this.valorAdquirido;
+  }
+
+  public void setValorAdquirido(Float valorAdquirido) {
+    this.valorAdquirido = valorAdquirido;
+  }
+
+  public Float getRetornoFinal() {
+    return this.retornoFinal;
+  }
+
+  public void setRetornoFinal(Float retornoFinal) {
+    this.retornoFinal = retornoFinal;
+  }
+
+}
+
---
+++ backend/src/main/java/br/com/fag/cassino/domain/dto/FichaDTO.java
@@ -0,0 +1,9 @@
+package br.com.fag.cassino.domain.dto;
+
+public class FichaDTO {
+  private final Float CUSTO = 5f;
+
+  public Float getCUSTO() {
+    return this.CUSTO;
+  }
+}
