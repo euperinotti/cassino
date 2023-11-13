71b00a811187c2124118d3fe15ad831e19d1482f
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-13 16:59:19 -0300

    refactor: code refactor

--- backend/src/main/java/br/com/fag/cassino/services/CassinoService.java
+++
@@ -1,9 +0,0 @@
-package br.com.fag.cassino.services;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
-public class CassinoService {
-  public void apostar(@RequestBody String text) {
-    System.out.println(text);
-  }
-}
