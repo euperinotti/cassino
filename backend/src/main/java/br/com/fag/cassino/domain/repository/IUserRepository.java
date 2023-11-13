c8a8543e1a0bc6e1f773b9a5ad08fa8613657377
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-13 16:59:51 -0300

    feat: add custom query method

--- backend/src/main/java/br/com/fag/cassino/domain/repository/IUserRepository.java
+++ backend/src/main/java/br/com/fag/cassino/domain/repository/IUserRepository.java
@@ -1,9 +1,14 @@
 package br.com.fag.cassino.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.com.fag.cassino.domain.entities.UserBO;
 
+@Repository
 public interface IUserRepository extends JpaRepository<UserBO, Long> {
-  
+  @Query(value = "SELECT u FROM UserBO u WHERE nome = :nome")
+  public UserBO findByNome(@Param("nome") String nome);
 }
