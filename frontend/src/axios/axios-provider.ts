3c7f322396af09de11cc98a27835091f8890347a
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-14 17:14:18 -0300

    feat: add axios routes

--- frontend/src/axios/axios-provider.ts
+++ frontend/src/axios/axios-provider.ts
@@ -41,6 +41,22 @@ export const buscarPorId = async (idUsuario: string) => {
   }
 }
 
+export const buscarCartas = async () => {
+  try {
+    const json = await axios.get(`${URL}/carta`)
+
+    return {
+      status: json.status,
+      data: json.data
+    }
+  } catch (error) {
+    return {
+      status: 400,
+      data: 'Não foi possível buscar cartas'
+    }
+  }
+}
+
 export const comprarFicha = async (idConta: number) => {
   try {
     const json = await axios.put(
@@ -64,6 +80,30 @@ export const comprarFicha = async (idConta: number) => {
     return {
       status: 400,
       data: 'Não foi possível comprar ficha'
+    }
+  }
+}
+
+export const registrarJogo = async (idUsuario: string, idCarta: string) => {
+  try {
+    const json = await axios.post(
+      `${URL}/jogo`,
+      JSON.stringify({ idUsuario, idCarta }),
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return {
+      status: json.status,
+      data: json.data
+    }
+  } catch (error) {
+    return {
+      status: 400,
+      data: 'Não foi possível cadastrar'
     }
   }
 }
