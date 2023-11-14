997de5680c403d6f2fa6b89f7a7d0930705f5f9a
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-14 14:36:41 -0300

    feat: add axios routes

--- frontend/src/axios/axios-provider.ts
+++ frontend/src/axios/axios-provider.ts
@@ -11,9 +11,53 @@ export const cadastrar = async (data: User) => {
       }
     })
 
-    return json.data
+    return {
+      status: json.status,
+      data: json.data
+    }
   } catch (error) {
-    return console.log('Não foi possível cadastrar')
+    return {
+      status: 400,
+      data: 'Não foi possível cadastrar'
+    }
+  }
+}
+
+export const buscarPorId = async (idUsuario: string) => {
+  try {
+    const json = await axios.get(`${URL}/user/${idUsuario}`)
+
+    return {
+      status: json.status,
+      data: json.data
+    }
+  } catch (error) {
+    return {
+      status: 400,
+      data: 'Não foi possível cadastrar'
+    }
+  }
+}
+
+export const comprarFicha = async (idConta: number) => {
+  try {
+    const json = await axios.put(
+      `${URL}/ficha`,
+      JSON.stringify({
+        idConta: idConta,
+        quantidade: 1
+      })
+    )
+
+    return {
+      status: json.status,
+      data: json.data
+    }
+  } catch (error) {
+    return {
+      status: 400,
+      data: 'Não foi comprar ficha'
+    }
   }
 }
 
--- frontend/src/components/AsideMenu/index.tsx
+++ frontend/src/components/AsideMenu/index.tsx
@@ -1,20 +1,41 @@
+import { useEffect, useState } from 'react'
 import { StatusField } from '../StatusField'
 import * as S from './styles'
+import * as axios from '../../axios/axios-provider'
 
 export const AsideMenu = () => {
+  const [ganho, setGanho] = useState<number>()
+  const [perda, setPerda] = useState<number>()
+  const [ficha, setFicha] = useState<number>()
+
+  useEffect(() => {
+    async function fetchData() {
+      const usuario = await axios.buscarPorId(
+        sessionStorage.getItem('idUsuario') as string
+      )
+      setGanho(usuario.data?.conta.saldoDeGanho)
+      setPerda(usuario.data?.conta.saldoDePerda)
+      setFicha(usuario.data?.conta.saldoDeFicha)
+    }
+
+    fetchData()
+  }, [])
+
+  console.log(perda)
+
   return (
     <S.Container>
       <StatusField
-        value="R$ Saldo de Ganho"
+        value={'R$ ' + ganho}
         title="Saldo de Ganho"
         statusType="GANHOU"
       />
       <StatusField
-        value="R$ Saldo de Perda"
+        value={'R$ ' + perda}
         title="Saldo de Perda"
         statusType="PERDEU"
       />
-      <StatusField value="Saldo de Perda" title="Quantidade de Fichas" />
+      <StatusField value={'R$ ' + ficha} title="Quantidade de Fichas" />
     </S.Container>
   )
 }
