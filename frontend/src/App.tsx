107baffbf8dc6ae4e5c547dfe6e8cf6bd2307838
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-14 00:38:47 -0300

    feat: add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,45 @@
+import { FormEvent, useState } from 'react'
+import * as axios from './axios/axios-provider'
+import { User } from './models/User'
+
 function App() {
-  return <h1>Hello World</h1>
+  const [nome, setNome] = useState<string>('')
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    if (nome != null) {
+      const data: User = {
+        id: null,
+        nome: nome,
+        conta: {
+          saldoDePerda: 0.0,
+          saldoDeGanho: 0.0,
+          saldoDeFichas: 0
+        }
+      }
+
+      const response = await axios.cadastrar(data)
+      console.log(response)
+    }
+  }
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nome"
+          id="nome"
+          value={nome}
+          onChange={(value) => setNome(value.target.value)}
+          placeholder="Insira seu nome"
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+    </>
+  )
 }
 
 export default App
--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
+import theme from './theme'
 
 export const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Open Sans', sans-serif;
+    src: url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap');
+  }
+
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap');;
+
   * {
     margin: 0;
     padding: 0;
@@ -8,8 +17,14 @@ export const GlobalStyles = createGlobalStyle`
   }
   
   body {
-    background-color: black;
+    background-color: ${theme.colors.black};
     color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    font-family: 'Open Sans';
   }
 
 `
