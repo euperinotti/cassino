b00115424d3b05b324712fcb55af4b0d9e94d1ae
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-14 12:21:00 -0300

    feat: add theme file

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,28 @@
+const theme = {
+  colors: {
+    black: '#151515',
+    red: '#a63d40',
+    lightRed: '#e3b5b7',
+    yellow: '#e19c3d',
+    lightYellow: '#ecc083',
+    green: '#89a253',
+    lightGreen: '#b7c893',
+    blue: '#6494aa',
+    white: '#F9F9F9'
+  },
+  fonts: {
+    family: {
+      poppins: 'Poppins'
+    },
+    weight: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extraBold: 800
+    }
+  }
+}
+
+export default theme
