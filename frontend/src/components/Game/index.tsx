b0159a04626ad8c16dc584970a5b11d2af24ba4b
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-14 13:59:33 -0300

    feat: add game component

---
+++ frontend/src/components/Game/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export const Game = () => {
+  return <S.Container>Hello</S.Container>
+}
---
+++ frontend/src/components/Game/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 600px;
+  height: 600px;
+  border: 2px solid red;
+`
